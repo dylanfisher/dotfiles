1e1c8c604387c3f45af6470c1c2119d9b95a3a95
dylanfisher <hi@dylanfisher.com>
2014-09-26 11:13:22 -0400

    emmet

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 #### Sublime Text
 
 `cd ~/Library/Application\ Support/Sublime\ Text\ 3`
+
+- AlignTab
+- All Autocomplete
+- BracketHighlighter
+- CTags
+- Emmet
+- ERB Snippets
+- GitGutter
+- Monokai Extended
+- Origami
+- Package Control
+- Sass
+- SideBarEnhancements
+- SublimeGit
+- SublimeRailsNav
--- sublime.sh
+++ sublime.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Copies Sublime Text settings to current directory.
 
-echo Copying Sublime Text files to your current directory;
+echo Replacing Sublime Text files in your current directory;
+rm -rf SublimeTextConfig;
 mkdir SublimeTextConfig;
 cp -R ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages ./SublimeTextConfig/;
 cp -R ~/Library/Application\ Support/Sublime\ Text\ 3/Packages ./SublimeTextConfig/;
